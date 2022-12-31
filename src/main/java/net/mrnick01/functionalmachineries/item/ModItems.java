99b0a30f31cd56637e348e36d0574678328422f0
nick <nicolobartolinii@gmail.com>
2022-12-31 17:17:11 +0100

    added items: Chrome Nugget, Chrome Tools; Added recipes for other items and blocks.

--- src/main/java/net/mrnick01/functionalmachineries/item/ModItems.java
+++ src/main/java/net/mrnick01/functionalmachineries/item/ModItems.java
@@ -16,6 +16,19 @@ public class ModItems {
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FM_TAB)));
     public static final RegistryObject<Item> RAW_CHROME_CHUNK = ITEMS.register("raw_chrome_chunk",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FM_TAB)));
+    public static final RegistryObject<Item> CHROME_NUGGET = ITEMS.register("chrome_nugget",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FM_TAB)));
+    public static final RegistryObject<Item> CHROME_PICKAXE = ITEMS.register("chrome_pickaxe",
+            () -> new Item(new Item.Properties().defaultDurability(512)));
+    public static final RegistryObject<Item> CHROME_SWORD = ITEMS.register("chrome_sword",
+            () -> new Item(new Item.Properties().defaultDurability(512)));
+    public static final RegistryObject<Item> CHROME_SHOVEL = ITEMS.register("chrome_shovel",
+            () -> new Item(new Item.Properties().defaultDurability(512)));
+    public static final RegistryObject<Item> CHROME_AXE = ITEMS.register("chrome_axe",
+            () -> new Item(new Item.Properties().defaultDurability(512)));
+    public static final RegistryObject<Item> CHROME_HOE = ITEMS.register("chrome_hoe",
+            () -> new Item(new Item.Properties().defaultDurability(512)));
+
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
